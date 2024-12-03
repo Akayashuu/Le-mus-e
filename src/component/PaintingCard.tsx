92ce74fa953d4eb6c46e9f0cbaf40cc1b298acad
Sauvage Léo <sauvageleo1@gmail.com>
2024-12-03 14:14:00 +0100

    fix: :bug: Card title wasn't correctly displayed

--- src/component/PaintingCard.tsx
+++ src/component/PaintingCard.tsx
@@ -7,13 +7,13 @@ type Painting = {
 function PaintingCard({ title, artist, image }: Painting) {
     return (
         <div className="flex">
-            <div className="w-80 h-96 rounded overflow-hidden shadow-lg">
+            <div className="w-96 h-96 rounded overflow-hidden shadow-lg">
                 <img
                     className="w-full h-3/4 object-cover"
                     src={image}
                     alt={title}
                 />
-                <div className="px-6 py-4 h-1/4">
+                <div className="py-2 px-2">
                     <div className="font-bold text-xl mb-2">{title}</div>
                     <p className="text-gray-700 text-base">{artist}</p>
                 </div>
